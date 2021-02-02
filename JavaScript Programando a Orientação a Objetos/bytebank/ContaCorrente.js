c8284a805cd811e55859c7610723ab6728341d52
Thiago Muniz <thiagotsocial@gmail.com>
2021-02-02 20:34:43 -0300

    dia 33

--- JavaScript Programando a Orientação a Objetos/bytebank/ContaCorrente.js
+++ JavaScript Programando a Orientação a Objetos/bytebank/ContaCorrente.js
@@ -2,11 +2,6 @@ import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente{
     static numeroDeContas = 0;
-    agencia;
-    _cliente;
-
-    // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
-     _saldo = 0;
     
 
     set cliente(novoValor){
@@ -27,6 +22,7 @@ export class ContaCorrente{
     constructor(agencia, cliente){
         this.agencia = agencia;
         this.cliente = cliente;
+        this._saldo = 0;
         ContaCorrente.numeroDeContas += 1;
     }
 
--- JavaScript Programando a Orientação a Objetos/bytebank/index.js
+++ JavaScript Programando a Orientação a Objetos/bytebank/index.js
@@ -1,14 +1,16 @@
-import {Cliente} from "./Cliente.js"
-import {ContaCorrente} from "./ContaCorrente.js"
+import { ContaPoupanca } from "./contaPoupanca.js";
+import {Cliente} from "./Cliente.js";
+import {ContaCorrente} from "./ContaCorrente.js";
+import {Conta} from "./Conta.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309 );
-const cliente2 = new Cliente("Alice", 88822233309);
 
-const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+const contaCorrenteRicardo = new Conta(0, cliente1 ,1001);
 contaCorrenteRicardo.depositar(500);
-const conta2 = new ContaCorrente(102, cliente2);
+contaCorrenteRicardo.sacar(100);
 
-let valor = 200;
-contaCorrenteRicardo.tranferir(valor, conta2);
+const contaPoupanca = new Conta(50, cliente1, 1001);
 
-console.log(ContaCorrente.numeroDeContas);
+console.log(contaPoupanca);
+
+console.log(contaCorrenteRicardo);
