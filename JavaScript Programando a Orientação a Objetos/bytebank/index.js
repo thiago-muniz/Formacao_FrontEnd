9a4fada7a3bc7f836a07f405b6921761ec3b50a6
Thiago Muniz <thiagotsocial@gmail.com>
2021-02-02 20:31:49 -0300

    dia 33

---
+++ JavaScript Programando a Orientação a Objetos/bytebank/index.js
@@ -0,0 +1,16 @@
+import { ContaPoupanca } from "./contaPoupanca.js";
+import {Cliente} from "./Cliente.js";
+import {ContaCorrente} from "./ContaCorrente.js";
+import {Conta} from "./Conta.js";
+
+const cliente1 = new Cliente("Ricardo", 11122233309 );
+
+const contaCorrenteRicardo = new Conta(0, cliente1 ,1001);
+contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(100);
+
+const contaPoupanca = new Conta(50, cliente1, 1001);
+
+console.log(contaPoupanca);
+
+console.log(contaCorrenteRicardo);
