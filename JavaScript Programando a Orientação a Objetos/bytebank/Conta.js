1aa0f69ae9cff4dfd95dda6589564fe89d9ac423
Thiago Muniz <thiagotsocial@gmail.com>
2021-02-03 10:55:06 -0300

    dia 34

--- JavaScript Programando a Orientação a Objetos/bytebank/Conta.js
+++ JavaScript Programando a Orientação a Objetos/bytebank/Conta.js
@@ -4,24 +4,37 @@ export class Conta{
         this._cliente = cliente;
         this._agencia = agencia;
     }
+
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+
+    get saldo(){
+        return this._saldo;
+    } 
+
     sacar(valor){
-        taxa = 1.1 * valor;
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
+        let taxa = 1;
+
+        const valorSacado = taxa * valor;
+        if(this._saldo >= valorSacado){
+            this._saldo -= valorSacado;
+            return valorSacado;
         }
     }
 
     depositar(valor){
-        if(valor <= 0)
-        {
-            return;
-        } 
         this._saldo += valor;           
     }
 
     tranferir(valor, conta){
-        
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
         
